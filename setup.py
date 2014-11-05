28c5b4eeca60f3cee8087995890a31e9d68ac9c4
Mikhail Baranov <mikhail.baranov@gmail.com>
2014-11-05 23:06:26 +0300

    Bump version to 0.1.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='django-sociallinks',
-    version='0.1.4',
+    version='0.1.5',
     packages=['sociallinks.templatetags'],
     url='https://github.com/h4/django-sociallinks/',
     license='MIT',
